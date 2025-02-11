59475085cb3e114bf221ddaf008fd6f20e7f1c19
Shirly Avrahamoff <shirly212@gmail.com>
2025-02-11 18:17:17 +0200

    Updated database structure and fixed test issues

--- routes/costs.js
+++ routes/costs.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Cost = require('../models/costs');
+const Cost = require('../models/cost');
 
 /**
  * @module routes/costs
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/users');
-const Cost = require('../models/costs');
+const User = require('../models/user');
+const Cost = require('../models/cost');
 
 /**
  * @module routes/users
